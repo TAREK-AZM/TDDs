584f5c6916ae065fac796b630ed6f766ed0d5ea9
tarek al azami <152443825+TAREK-AZM@users.noreply.github.com>
2026-04-01 11:22:28 +0100

    feat: add the TDD training of kent beck book

---
+++ TDDByExample/MoneyTDD-KentBeck/MoneyTest.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+
+namespace TDDByExample
+{
+    public class MoneyTest
+    {
+
+
+        [Fact]
+        public void Doller_mulrtiplication()
+        {
+            Money five = Money.doller(5);
+
+            Assert.Equal(Money.doller(10), five.Times(2));
+            Assert.Equal(Money.doller(15), five.Times(3));
+        }
+
+        [Fact]
+        public void Franc_multiplication()
+        {
+            Money five = Money.franc(5);
+
+            Assert.Equal(Money.franc(10), five.Times(2));
+            Assert.Equal(Money.franc(15), five.Times(3));
+        }
+
+        [Fact]
+        public void Test_equality()
+        {
+            Assert.True(Money.doller(5).Equals(Money.doller(5)));
+            Assert.False(Money.doller(5).Equals(Money.doller(6)));
+            Assert.False(Money.franc(5).Equals(Money.doller(5)));
+        }
+
+        [Fact]
+        public void Test_different_class_equality()
+        {
+            Assert.False(new Money(10, "USA").Equals(new Franc(10, "CHF")));
+        }
+
+        [Fact]
+        public void Test_curency()
+        {
+            Assert.True("USD".Equals(Money.doller(1).Currency()));
+        }
+
+
+    }
+}
---
+++ TDDByExample/MoneyTDD-KentBeck/Sum.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace TDDByExample
+{
+    public class Sum : IExpression
+    {
+        public IExpression? Augend { get;  set; }
+        public IExpression? Addend { get;  set; }
+        public Sum(IExpression augend,IExpression addend) { 
+            Augend = augend;
+            Addend = addend;
+        }
+        public Money Reduce(Bank bank, string to) 
+        {
+            var changedAugend = Augend.Reduce(bank,to); // if Augend is Sum : calculated recursively
+            var changedAddend = Addend.Reduce(bank,to);
+
+            var total = changedAugend.Amount + changedAddend.Amount;
+            return new Money (total,to);
+        }
+        public IExpression Plus(IExpression addend)
+        {
+            return new Sum(this,addend);
+        }
+        public  IExpression Times(int multiplayer)
+        {
+           return new Sum(Augend.Times(multiplayer),Addend.Times(multiplayer));
+        }
+    }
+}
