26e8ac7d9ffe3f081e6fe614601cf7ab0610297f
tarek al azami <152443825+TAREK-AZM@users.noreply.github.com>
2026-04-01 12:02:50 +0100

    add core folder

---
+++ BankAccount.Core/Domain/Transaction.cs
@@ -0,0 +1,42 @@
+﻿namespace BankAccountKataTDD
+{
+    public class Transaction
+    {
+        public string Date { get; }
+        public int Amount { get; }
+        public int Balance { get; }
+
+
+        public Transaction(string date,int amount,int balance)
+        {
+            Date = date;
+
+            Amount = amount;
+
+            Balance = balance;
+        }
+
+        public override string ToString()
+        {
+            return $"{Date} || {Amount}   || {Balance}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj == null) return false;
+            if (obj is Transaction transaction) { 
+               return Date.Equals(transaction.Date) 
+                    && Amount.Equals(transaction.Amount) 
+                    && Balance.Equals(transaction.Balance);
+            }
+            return false;
+        }
+
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+     
+    }
+}
