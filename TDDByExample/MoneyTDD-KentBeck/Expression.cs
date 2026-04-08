3cef8fb0ea740b28a84c4140368a96388db413a2
tarek al azami <152443825+TAREK-AZM@users.noreply.github.com>
2026-04-08 16:48:49 +0100

    remove all

--- TDDByExample/MoneyTDD-KentBeck/Expression.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TDDByExample
-{
-    public interface IExpression
-    {
-        public Money Reduce(Bank bank,string to);
-        public IExpression Plus(IExpression addend);
-        IExpression Times(int multiplayer);
-    }
-}
